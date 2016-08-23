5b89a172b02a8dbedfbae8670cca5039424851e5
GianhTran <tran.nguyen.song.gianh@framgia.com>
2016-08-23 13:03:31 +0700

    create UI of home screen

---
+++ app/src/main/java/com/example/framgia/t1_rss_feed/Constants.java
@@ -0,0 +1,9 @@
+package com.example.framgia.t1_rss_feed;
+
+/**
+ * Copyright @ 2016 Framgia inc
+ * Created by GianhTNS on 23/08/2016.
+ */
+public class Constants {
+    public static final String TAG = "gianhTran";
+}
